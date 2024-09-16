3174b97936a190c4af6d9e53a3d8c69fcf516679
abdoaldoush <141569025+abdoaldoush@users.noreply.github.com>
2024-09-16 22:38:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("openFacebookProfile").addEventListener("click", function() {
+    window.open("https://www.facebook.com/aldoushy.mahdy", "_blank"); // Open link in a new tab
+});
+
+
+
+document.getElementById("openinstagramProfile").addEventListener("click", function() {
+    window.open("https://www.instagram.com/aldoushy/", "_blank"); // Open link in a new tab
+});
