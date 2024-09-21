9eb91e2e40e84b1450079d1fa5049ab67b283f8b
abdoaldoush <141569025+abdoaldoush@users.noreply.github.com>
2024-09-21 20:25:14 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("openFacebookProfile").addEventListener("click", function() {
-    window.open("https://www.facebook.com/aldoushy.mahdy", "_blank"); // Open link in a new tab
-});
-
-
-
-document.getElementById("openinstagramProfile").addEventListener("click", function() {
-    window.open("https://www.instagram.com/aldoushy/", "_blank"); // Open link in a new tab
-});
